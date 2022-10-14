7f7262847cabc05964704cd0cafcaae16941a7d9
Vladyslav <vkadyrov@protonmail.com>
2022-10-14 19:37:11 -0400

    commit tree 6a56ea06eb2aee609cb41262f6e403534f06e8ba

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
+// function hasTargetSum(array, target) {
+//   // Write your algorithm here
+//   for (let i = 0; i < array.length; i++) {
+//     const comp = target - array[i];
+//     for (let j = i + 1; j < array.length; j++) {
+//       if (array[j] === comp) return true
+//     }
+//   }
+//   return false
+// }
+
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  const seenNumbers = {};
+  
+  for (const number of array) {
+    console.log(seenNumbers)
+    const comp = target - number;
+    if (comp in seenNumbers) return true;
+    seenNumbers[number] = true;
+  }
+  return false;
 }
+
+
 
 /* 
   Write the Big O time complexity of your function here
@@ -8,10 +29,22 @@ function hasTargetSum(array, target) {
 
 /* 
   Add your pseudocode here
+  
+  a = 1st elevemnt of the array;
+  array2 = spread array
+  remove first element of array2
+  for b in array2
+  if (a+b) === target
+    return true
+    else
+      return false
 */
 
 /*
   Add written explanation of your solution here
+
+  We need compare sum of any two numbers in the array to target value;
+  If any sum equiles target number, function will return true.
 */
 
 // You can run `node index.js` to view these console logs
